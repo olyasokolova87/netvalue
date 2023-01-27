6674fc9ff7bc69c4298270b43d87832b894779ed
smirnovaov <olyasokolova87@gmail.com>
2023-01-28 00:34:32 +0300

    Create initial structure

---
+++ src/main/java/nz/netvalue/ChargePointsApplication.java
@@ -0,0 +1,11 @@
+package nz.netvalue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargePointsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChargePointsApplication.class, args);
+    }
+}
---
+++ src/test/java/nz/zealand/chalenge/ChargePointsApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.zealand.chalenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargePointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
